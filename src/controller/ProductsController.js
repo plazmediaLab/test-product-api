e2166e89e4c472449466acde757f53448e4c560f
AdrianGD1337 <contacto.plazmedia@gmail.com>
2020-09-16 15:37:09 -0500

    [MODIFY] Controlador Product refactorizado con Try-Catch

--- src/controller/ProductsController.js
+++ src/controller/ProductsController.js
@@ -3,25 +3,43 @@ import Product from '../models/Products';
 class ProductsController {
   async store(req, res) {
 
-    const { name, category, price, imgURL } = req.body;
+    try {
+      // Destructuring info
+      const { name, category, price, imgURL } = req.body;
+      // New model instance
+      const newDoc = new Product({
+        name,
+        category,
+        price,
+        imgURL
+      });
+      // Create doc
+      const doc = await newDoc.save();
+      // Response
+      return res.status(201).json(doc); 
 
-    const newDoc = new Product({
-      name,
-      category,
-      price,
-      imgURL
-    });
-
-    const doc = await newDoc.save();
-
-    return res.status(201).json(doc); 
+    } catch (error) {
+      // Error response message
+      const resMsn = { error: 409, message: error.message};
+      // Response
+      console.log(resMsn);
+      return res.status(409).json(resMsn);
+    }
   }
 
   async index(req, res) {
-
-    const docs = await Product.find();
-    
-    return res.status(200).json(docs);
+    try {
+      // Search docs on DB
+      const docs = await Product.find();
+      // Success response
+      return res.status(200).json(docs);
+    } catch (error) {
+      // Error response message
+      const resMsn = { error: 404, message: error.message};
+      // Response
+      console.log(resMsn);
+      return res.status(404).json(resMsn);
+    }
   }
 
   async create(req, res) {
@@ -29,10 +47,24 @@ class ProductsController {
   }
 
   async show(req, res) {
+    try {
 
-    const doc = await Product.findById(req.params.id);
-    
-    return res.status(200).json(doc);
+      // Doc to search
+      const doc = await Product.findById(req.params.id);
+      // Document existence validation
+      if(!doc){
+        throw new Error('Item no encontrado en la base de datos');
+      }
+      // Success response
+      return res.status(200).json(doc);
+      
+    } catch (error) {
+      // Error response message
+      const resMsn = { error: 404, message: error.message};
+      // Response
+      console.log(resMsn);
+      return res.status(404).json(resMsn);
+    }
   }
 
   async edit(req, res) {
@@ -40,18 +72,47 @@ class ProductsController {
   }
 
   async update(req, res) {
+    try {
 
-    const doc = await Product.findByIdAndUpdate(req.params.id, req.body, {
-      new: true
-    })
+      // Doc to search and update
+      const doc = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+      })
+      // Document existence validation
+      if(!doc){
+        throw new Error('Item no encontrado en la base de datos');
+      }
+      // Success response
+      return res.status(200).json(doc);
 
-    return res.status(200).json(doc);
+    } catch (error) {
+      // Error response message
+      const resMsn = { error: 404, message: error.message};
+      // Response
+      console.log(resMsn);
+      return res.status(404).json(resMsn);
+    }
   }
 
   async destroy(req, res) {
-    const doc = await Product.findByIdAndDelete(req.params.id)
+    try {
 
-    return res.status(204).json(doc);
+      // Doc to search and delete
+      const doc = await Product.findByIdAndDelete(req.params.id)
+      // Document existence validation
+      if(!doc){
+        throw new Error('Item no encontrado en la base de datos');
+      }
+      // Succes response
+      return res.status(200).json({ message: 'Item removed successfully' });
+      
+    } catch (error) {
+      // Error response message
+      const resMsn = { error: 404, message: error.message};
+      // Response
+      console.log(resMsn);
+      return res.status(404).json(resMsn);
+    }
   }
 
   async view(req, res) {
