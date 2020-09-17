a67bc162dfe8e8c26ed7ed11918a71111d01d9b2
AdrianGD1337 <contacto.plazmedia@gmail.com>
2020-09-17 18:11:24 -0500

    [ADD] Validación de Rutas con roles ADMIN, MODERATOR e USER

--- src/middlewares/authJWT.js
+++ src/middlewares/authJWT.js
@@ -3,23 +3,29 @@ import { SECRET_WORD } from '../config';
 import User from '../models/User';
 import Role from '../models/Role';
 
+/**
+ * Verifica existencia de Token en Headers + Validación Signature
+ *
+ * @param  \jwt
+ * @return False -> Error message or True -> Next function
+ */
 export const verifyToken = (req, res, next) => {
 
   const token = req.headers["x-access-token"];
 
-  if(!token) return res.status(403).json({error: 403, message: "Access TOKEN not received"});
+  if(!token) return res.status(403).json({error: 403, message: "Access TOKEN not received."});
 
   jwt.verify(token, SECRET_WORD, async (err, decoded) => {      
     
     if (err) {
-      return res.status(401).json({ error: 401, mensaje: 'Token signature inválida' });    
+      return res.status(401).json({ error: 401, mensaje: 'Token signature inválida.' });    
     }
     
     const decodeInfo = decoded;
     req.id = decodeInfo.id
 
     const user = await User.findById(req.id);
-    if(!user) return res.status(401).json({ error: 404, mensaje: 'Info no found' });
+    if(!user) return res.status(401).json({ error: 404, mensaje: 'Info no found.' });
 
     next();
 
@@ -27,10 +33,33 @@ export const verifyToken = (req, res, next) => {
 
 };
 
+/**
+ * Valida role ADMIN
+ *
+ * @param  \role
+ * @return False -> Error message or True -> Next function
+ */
 export const isAdmin = async (req, res, next) => {
-  console.log('This user is ADMIN');
+  const user = await User.findById(req.id);
+  const roles = await Role.find({_id: {$in: user.roles}});
+
+  for (let i = 0; i < roles.length; i++) {
+    if(roles[i].name === 'ADMIN_ROLE'){
+      next();
+      return
+    }
+  }
+
+  return res.status(403).json({ error: 403, mensaje: 'Administrator permission is required to perform this action.' });
+
 }
 
+/**
+ * Valida role MODERATOR
+ *
+ * @param  \role
+ * @return False -> Error message or True -> Next function
+ */
 export const isModerator = async (req, res, next) => {
   const user = await User.findById(req.id);
   const roles = await Role.find({_id: {$in: user.roles}});
@@ -42,6 +71,6 @@ export const isModerator = async (req, res, next) => {
     }
   }
 
-  return res.status(403).json({ error: 403, mensaje: 'Moderator permission is required to perform this action' });
-  
+  return res.status(403).json({ error: 403, mensaje: 'Moderator permission is required to perform this action.' });
+
 }
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -10,7 +10,7 @@ const routes = new Router();
 routes.get('/', ProductController.index);// -> Obtener productos
 routes.post('/', [authJWT.verifyToken, authJWT.isModerator], ProductController.store);// -> Crear producto
 routes.get('/:id', ProductController.show);// -> obtenrr un producto por ID
-routes.put('/:id', [authJWT.verifyToken],  ProductController.update);// -> Actualizar un producto
-routes.delete('/:id', [authJWT.verifyToken, authJWT.isModerator],  ProductController.destroy);// -> Eliminar un producto
+routes.put('/:id', [authJWT.verifyToken, authJWT.isAdmin],  ProductController.update);// -> Actualizar un producto
+routes.delete('/:id', [authJWT.verifyToken, authJWT.isAdmin],  ProductController.destroy);// -> Eliminar un producto
 
 module.exports = routes;
