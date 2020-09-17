da12c91ac564258601afa85b916d6b787d9a1a4b
AdrianGD1337 <contacto.plazmedia@gmail.com>
2020-09-17 17:46:02 -0500

    [ADD] Validacion de MODERATOR_ROLE en rutas Store y Destroy

--- src/middlewares/authJWT.js
+++ src/middlewares/authJWT.js
@@ -1,8 +1,10 @@
 import jwt from 'jsonwebtoken';
 import { SECRET_WORD } from '../config';
 import User from '../models/User';
+import Role from '../models/Role';
 
 export const verifyToken = (req, res, next) => {
+
   const token = req.headers["x-access-token"];
 
   if(!token) return res.status(403).json({error: 403, message: "Access TOKEN not received"});
@@ -14,8 +16,9 @@ export const verifyToken = (req, res, next) => {
     }
     
     const decodeInfo = decoded;
+    req.id = decodeInfo.id
 
-    const user = await User.findById(decodeInfo.id);
+    const user = await User.findById(req.id);
     if(!user) return res.status(401).json({ error: 404, mensaje: 'Info no found' });
 
     next();
@@ -23,3 +26,22 @@ export const verifyToken = (req, res, next) => {
   });
 
 };
+
+export const isAdmin = async (req, res, next) => {
+  console.log('This user is ADMIN');
+}
+
+export const isModerator = async (req, res, next) => {
+  const user = await User.findById(req.id);
+  const roles = await Role.find({_id: {$in: user.roles}});
+
+  for (let i = 0; i < roles.length; i++) {
+    if(roles[i].name === 'MODERATOR_ROLE'){
+      next();
+      return
+    }
+  }
+
+  return res.status(403).json({ error: 403, mensaje: 'Moderator permission is required to perform this action' });
+  
+}
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { verifyToken } from '../middlewares';
+import { authJWT } from '../middlewares';
 
 // import all controllers
 import ProductController from '../controller/ProductsController';
@@ -8,9 +8,9 @@ const routes = new Router();
 
 // Add routes
 routes.get('/', ProductController.index);// -> Obtener productos
-routes.post('/', verifyToken, ProductController.store);// -> Crear producto
+routes.post('/', [authJWT.verifyToken, authJWT.isModerator], ProductController.store);// -> Crear producto
 routes.get('/:id', ProductController.show);// -> obtenrr un producto por ID
-routes.put('/:id', verifyToken,  ProductController.update);// -> Actualizar un producto
-routes.delete('/:id', verifyToken,  ProductController.destroy);// -> Eliminar un producto
+routes.put('/:id', [authJWT.verifyToken],  ProductController.update);// -> Actualizar un producto
+routes.delete('/:id', [authJWT.verifyToken, authJWT.isModerator],  ProductController.destroy);// -> Eliminar un producto
 
 module.exports = routes;
